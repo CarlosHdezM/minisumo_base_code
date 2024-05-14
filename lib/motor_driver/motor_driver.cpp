2d5b70cb5f4122060423d0cfe386bd90114a6497
CarlosHdezM <64612604+CarlosHdezM@users.noreply.github.com>
2024-05-14 12:08:35 +0800

    Add "motor driver" simple driver

---
+++ lib/motor_driver/motor_driver.cpp
@@ -0,0 +1,24 @@
+#include "motor_driver.hpp"
+
+
+
+void MotorDriver::initialize()
+{
+    pinMode(this->pin_1_, OUTPUT);
+    pinMode(this->pin_2_, OUTPUT);
+    digitalWrite(this->pin_1_, LOW);
+    digitalWrite(this->pin_2_, LOW);
+}
+
+
+void MotorDriver::setVelocity(int16_t velocity) //From -255 to 255 (8 bit resolution PWM)
+{
+    if(velocity > 0){
+        digitalWrite(this->pin_1_, HIGH);
+        analogWrite(this->pin_2_, 255-velocity);
+    }
+    else{
+        analogWrite(this->pin_1_, 255+velocity);
+        digitalWrite(this->pin_2_, HIGH);
+    }
+}
---
+++ lib/motor_driver/motor_driver.hpp
@@ -0,0 +1,18 @@
+#ifndef ROBOT_MOTOR_DRIVER_H
+#define ROBOT_MOTOR_DRIVER_H
+
+#include "Arduino.h"
+
+class MotorDriver
+{
+    public:
+        MotorDriver(uint8_t pin_1, uint8_t pin_2) : pin_1_(pin_1), pin_2_(pin_2){};
+        void initialize();
+        void setVelocity(int16_t velocity); // From -255 to 255 (8 bit resolution PWM)
+
+    private:
+        const uint8_t pin_1_;
+        const uint8_t pin_2_;
+};
+
+#endif
