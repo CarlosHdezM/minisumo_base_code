82ea78e8b0a83358157394a5f60e3e1325f38bbf
CarlosHdezM <64612604+CarlosHdezM@users.noreply.github.com>
2024-05-16 16:43:44 +0800

    Change line sensors threshold value

--- include/robot_line_sensors_settings.h
+++ include/robot_line_sensors_settings.h
@@ -3,8 +3,8 @@
 #include "stdint.h"
 
 namespace line_sensor_settings{
-    constexpr uint16_t left_threshold_white{200};
-    constexpr uint16_t right_threshold_white{200};
+    constexpr uint16_t left_threshold_white{350};
+    constexpr uint16_t right_threshold_white{350};
 }
 
 #endif
