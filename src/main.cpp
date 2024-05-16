3239ba85664efa8ff832c5a13bacff70f975b767
CarlosHdezM <64612604+CarlosHdezM@users.noreply.github.com>
2024-05-16 16:46:34 +0800

    Add State Machine usage in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include "robot_line_sensors_settings.h"
 #include "opponent_sensor.hpp"
 #include "minisumo_robot.h"
+#include "dip_switch.hpp"
+#include "state_machine.h"
 
 
 //Globals - Hardware resources.
+uint8_t DIP_SW[] = {robot_pins::DIP_SW_BIT_0, robot_pins::DIP_SW_BIT_1, robot_pins::DIP_SW_BIT_2};
 MinisumoRobot robot{
     robot_pins::MOTOR_LEFT_1, robot_pins::MOTOR_LEFT_2,
     robot_pins::MOTOR_RIGHT_1, robot_pins::MOTOR_RIGHT_2,
@@ -18,8 +21,12 @@ MinisumoRobot robot{
     robot_pins::IR_SENSOR_M,
     robot_pins::IR_SENSOR_RM,
     robot_pins::IR_SENSOR_R,
-    robot_pins::START_MODULE
+    robot_pins::START_MODULE,
+    DipSwitch<robot_pins::DIP_SW_NUM_BITS>(DIP_SW),
+    robot_pins::BUZZER
 };
+
+RobotSM::State current_state{RobotSM::State::STOPPED};
 
 
 //Globals - Global variables
@@ -27,21 +34,64 @@ MinisumoRobot robot{
 
 void setup()
 {
-    robot.initialize();
     //Test Setup
     Serial.begin(115200);
+
+    robot.initialize();
 
     pinMode(robot_pins::SPARE_PIN, OUTPUT);
     digitalWrite(robot_pins::SPARE_PIN, LOW);
 }
 
 
+
 void loop()
 {
-    digitalWrite(robot_pins::SPARE_PIN, !digitalRead(robot_pins::SPARE_PIN));
-    //volatile int val = read_robot_sensors();
-    robot.motion(255,255,100,0b0,0b0);
-    //Serial.println(val, BIN);
-    //delay(50);
-}
+    
+    switch (current_state)
+    {
+        case RobotSM::State::STOPPED:
+            current_state = RobotSM::stopped(robot);
+            break;
 
+        case RobotSM::State::RETREAT:
+            current_state = RobotSM::retreat(robot);
+            break;
+
+        case RobotSM::State::SEARCHING:
+            current_state = RobotSM::searching(robot);
+            break;
+
+        case RobotSM::State::PRE_ATTACKING:
+            current_state = RobotSM::pre_attack(robot);
+            break;
+
+        case RobotSM::State::ATTACKING:
+            current_state = RobotSM::attack(robot);
+            break;
+
+
+        default:
+            Serial.print("ERROR: WE SHOULDN'T REACH THIS CASE.");
+            break;
+    }
+
+
+    digitalWrite(robot_pins::SPARE_PIN, !digitalRead(robot_pins::SPARE_PIN));
+    Serial.println("HEY");
+    //volatile int val = robot.();
+    //robot.motion(255,255,100,0b0,0b0);
+    //Serial.println(val, BIN);
+    //volatile uint8_t val2 = robot.read_dip_sw();
+    //auto response{robot.read_sensors()};
+    //volatile auto result = robot.motion(255,255,0,0b0,0b0);
+    //volatile auto result = robot.read_sensors();
+    //response.line_sensors = 3;
+    //Serial.print(response.line_sensors);
+    //Serial.print("\t");
+    //Serial.print(response.opponent_sensors);
+    //Serial.print("\t");
+    //Serial.print(response.start_module);
+    //Serial.println(val2);
+   // Serial.println();
+}
